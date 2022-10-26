293527ef9e9b315356470683e4b1422ceaf6acf0
hoon2 <sanghoon.engineer@gmail.com>
2022-10-27 07:46:01 +0900

    grid 초기코드

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import GlobalStyle from "./GlobalStyle";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+const Parent = styled.div`
+  width: 80%;
+  background-color: teal;
+`;
+const Child = styled.div`
+  font-size: 18px;
+  text-align: center;
+  height: 100px;
+  box-sizing: border-box;
+  color: white;
+  background-color: dodgerblue;
+  border: 1px solid black;
+`;
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <div>안녕</div>
+      <Container>
+        <Parent>
+          <Child>child 1</Child>
+          <Child>child 2</Child>
+          <Child>child 3</Child>
+          <Child>child 4</Child>
+          <Child>child 5</Child>
+          <Child>child 6</Child>
+        </Parent>
+      </Container>
     </>
   );
 }
